bc4aefa2351cb76f1d998030f16bb037e5ddf7ee
sanderson <cbsanderson.04@gmail.com>
2024-09-22 15:12:16 -0300

    AT 5

---
+++ src/05/App.jsx
@@ -0,0 +1,27 @@
+import styles from "./styles.module.css";
+
+export default function App() {
+    return (
+        <div className={styles.page}>
+            <header className={styles.header}>
+                <h2>Header</h2>
+            </header>
+            <nav className={styles.nav}>
+                <span className={styles.menuToggle}> ☰ </span>
+                <h2>Global Menu</h2>
+            </nav>
+            <section className={styles.ads}>
+                <h2>Ads</h2>
+            </section>
+            <section className={styles.context}>
+                <h2>Context Menu</h2>
+            </section>
+            <section className={styles.main}>
+                <h2>Main Content</h2>
+            </section>
+            <footer className={styles.footer}>
+                <h2>Footer</h2>
+            </footer>
+        </div>
+    )
+}
