2eaa97bccdc1e55c8de699e103f239a62eb10599
sanderson <cbsanderson.04@gmail.com>
2024-09-23 15:14:46 -0300

    AT 7 complete

--- src/07/Card/Card.jsx
+++ src/07/Card/Card.jsx
@@ -1,13 +1,51 @@
 import { FaPhone, FaEnvelope, FaLocationDot } from "react-icons/fa6";
 import styled from "styled-components";
 
-const sectionAlign = styled.section`
-width:100vw;
+const SectionAlignRow = styled.section`
+width:100%;
+padding: 10px;
 display:flex;
 flex-direction:row;
 justify-content:space-evenly;
 font-size:2rem;
-`;
+border:1px solid black;
+`
+
+const SectionColumn = styled.section`
+width:100%;
+padding:20px;
+font-size:2rem;
+`
+
+const Img = styled.img`
+width:100%;
+`
+
+const PersonalInformation = styled.h3`
+font-size:1.5rem;
+font-weigth:600;
+`
+
+const Table = styled.table`
+border:1px solid black;
+width: calc(100% - 20px);
+margin: 10px;
+border-collapse:collapse;
+`
+
+const CellHead = styled.td`
+border:1px solid black;
+padding:5px;
+text-align:center;
+font-weight: 800;
+background-color: grey;
+`
+
+const CellBody = styled.td`
+border:1px solid black;
+padding:5px;
+text-align:center;
+`
 
 export default function Card(props) {
     const image = {
@@ -17,30 +55,44 @@ export default function Card(props) {
     return (
         <>
             <section>
-                <img style={image} src={props.data.image} alt="Foto" />
+                <Img style={image} src={props.data.image} alt="Foto" />
             </section>
-            <section>
+            <SectionColumn>
                 <h2>{props.data.name}</h2>
-                <h3>{props.data.date}</h3>
-                <h3>{props.data.sector}</h3>
-                <h3>{props.data.position}</h3>
-            </section>
+                <PersonalInformation>{props.data.date}</PersonalInformation>
+                <PersonalInformation>{props.data.sector}</PersonalInformation>
+                <PersonalInformation>{props.data.position}</PersonalInformation>
+            </SectionColumn>
 
-            <sectionAlign>
-                <span><FaPhone /></span>
-                <span><FaEnvelope /></span>
-                <span><FaLocationDot /></span>
-            </sectionAlign>
+            <SectionAlignRow>
+                <FaPhone />
+                <FaEnvelope />
+                <FaLocationDot />
+            </SectionAlignRow>
 
             <section>
-                <table>
+                <Table>
                     <thead>
-
+                        <tr>
+                            <CellHead>Estado</CellHead>
+                            <CellHead>Tarefas</CellHead>
+                        </tr>
                     </thead>
                     <tbody>
-
+                        <tr>
+                            <CellBody><input type="checkbox"/></CellBody>
+                            <CellBody>Tarefa 1</CellBody>
+                        </tr>
+                        <tr>
+                            <CellBody><input type="checkbox"/></CellBody>
+                            <CellBody>Tarefa 2</CellBody>
+                        </tr>
+                        <tr>
+                            <CellBody><input type="checkbox"/></CellBody>
+                            <CellBody>Tarefa 2</CellBody>
+                        </tr>
                     </tbody>
-                </table>
+                </Table>
             </section>
 
         </>
